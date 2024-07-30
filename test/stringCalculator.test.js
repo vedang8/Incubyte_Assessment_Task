0c1134c3d240a633ae173ab675dfddd66f8adba3
Vedang Shailesh Joshi <vedang2607@gmail.com>
2024-07-30 18:41:42 +0530

    Test Case: Long Custom Delimeters implemented

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -73,4 +73,12 @@ describe('StringCalculator', () => {
         expect(calculator.add("//[***][%%][&]\n1***2%%3&3")).toBe(9);
         expect(calculator.add("//[@@][;;][###]\n1@@1;;2###3\n3")).toBe(10);
     });
+
+    // Complex Test Case: Long Custom Delimeters
+    test('Input: Numbers separated with the long custom delimeters | Output: Sum of numbers', () => {
+        const calculator = new StringCalculator();
+        expect(calculator.add("//[******]\n1******2******3******\n3")).toBe(9);
+    });
+
+
 });
